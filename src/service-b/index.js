f18b1cc8d7f082190215d7a0f5ef975f517bb01e
Aritra Ghosh <aritrag94@gmail.com>
2024-12-06 18:19:36 -0800

    fixed b

--- src/service-b/index.js
+++ src/service-b/index.js
@@ -2,19 +2,19 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
-const serviceBUrl = process.env.SERVICE_B_URL || 'http://service-b.microservices-demo.svc.cluster.local:3000';
+const serviceCUrl = process.env.SERVICE_C_URL || 'http://service-c.microservices-demo.svc.cluster.local:3000';
 
 app.get('/', async (req, res) => {
   try {
-    const response = await axios.get(serviceBUrl);
-    res.send(`Service A received: ${response.data}`);
+    const response = await axios.get(serviceCUrl);
+    res.send(`Service B received: ${response.data}`);
   } catch (error) {
-    console.error('Service A: Error calling Service B:', error.message);
-    res.status(500).send('Service B failed, propagating error.');
+    console.error('Service B: Error calling Service C:', error.message);
+    res.status(500).send('Service C failed, propagating error.');
   }
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Service A running on port ${PORT}`);
+  console.log(`Service B running on port ${PORT}`);
 });
